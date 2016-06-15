aad111474826f0baff20affce8d3a6a860582c61
Fspiga13 <nadril@tiscali.it>
2016-06-15 16:50:49 +0200

    Update content.php

--- amm2015/php/view/login/content.php
+++ amm2015/php/view/login/content.php
@@ -2,14 +2,14 @@
     <h3>Login</h3>
 
     <form method="post" action="login">
-        <input type="hidden" name="cmd" value="login"/>
+        <input type="hidden" name="cmd" value="index.php?page=login"/>
         <label for="user">Username</label>
         <input type="text" name="user" id="user"/>
         <br>
         <label for="password">Password</label>
         <input type="password" name="password" id="password"/> 
         <br/>
-        <input type="submit" value="index.php?page=login"/>
+        <input type="submit" value="Login"/>
        
 
     </form>
